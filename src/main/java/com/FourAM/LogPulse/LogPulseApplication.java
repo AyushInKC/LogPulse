a0f0ad6b8587badd6a530c1d0eee8ece19c0d434
AyushInKC <sirfayush1008@gmail.com>
2025-09-30 23:30:43 +0530

    Project SETUP

---
+++ src/main/java/com/FourAM/LogPulse/LogPulseApplication.java
@@ -0,0 +1,13 @@
+package com.FourAM.LogPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FourAM/LogPulse/LogPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FourAM.LogPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
