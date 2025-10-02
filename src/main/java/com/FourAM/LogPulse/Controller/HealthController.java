d1d31049f736703a574c2694e3a86f662515a1ab
AyushInKC <sirfayush1008@gmail.com>
2025-10-02 21:23:23 +0530

    ADDED A HEALTH CONTROLLER

---
+++ src/main/java/com/FourAM/LogPulse/Controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.FourAM.LogPulse.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/check")
+    public ResponseEntity<?> getHealth(){
+        return ResponseEntity.ok("Health is Good");
+    }
+}
